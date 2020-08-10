ee37b74662b8775afeaaa2d358b31c9fe92a04cc
Alvar San Martin <alvarsnow@gmail.com>
2020-08-10 02:33:28 +0200

    primer twit

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 let Twit = require('twit');
 let Twitter = new Twit(require('./config.js'));
 
-/**
- * Retweets a tweet passed into the function.
- * @param {*} tweet
- */
-function retweet(tweet) {
-    Twitter.post(
-        'statuses/retweet/:id',
-        { id: tweet.id_str },
-        function (err, data, response) {
-            // do something
-        });
+function main() {
+    Twitter.post('statuses/update', { status: 'Hola lucia' }, function (err, data, response) {
+        console.log(data)
+    })
 }
+
+// Inicio
+main();
+
+// intervalo de repeticion (en ms)
+setInterval(main, 5000);
